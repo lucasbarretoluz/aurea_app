235295db57c453434d06d11b33226f55c4b965bd
Lucas Barreto <lucasbarretoluz@hotmail.com>
2025-12-04 20:11:29 -0300

    feat: integrate Firebase configuration and update app initialization in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:aurea_app/src/aurea_app.dart';
+import 'package:aurea_app/src/core/firebase/firebase_options.dart';
 import 'package:aurea_app/src/data/repository/auth_repository.dart';
 import 'package:aurea_app/src/data/repository/firebase_auth_repository.dart';
 import 'package:aurea_app/src/data/repository/profile_user_repository.dart';
@@ -6,6 +7,7 @@ import 'package:aurea_app/src/data/repository/token_repository.dart';
 import 'package:aurea_app/src/logic/bloc/auth/auth_bloc.dart';
 import 'package:aurea_app/src/logic/cubit/app_bloc_observer.dart';
 import 'package:aurea_app/src/presentation/router/router.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
@@ -17,7 +19,13 @@ final GetIt getIt = GetIt.instance;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   await dotenv.load(fileName: '.env.dev');
+
+  getIt.registerSingleton<AuthRepository>(AuthRepository());
+  getIt.registerSingleton<TokenRepository>(TokenRepository());
+  getIt.registerSingleton<ProfileUserRepository>(ProfileUserRepository());
 
   getIt.registerSingleton<AuthBloc>(
     AuthBloc(
