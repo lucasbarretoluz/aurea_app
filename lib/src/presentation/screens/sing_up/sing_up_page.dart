e846b216f62071a501979e6c0491e740ec43df53
Lucas Barreto <lucasbarretoluz@hotmail.com>
2025-12-07 08:56:58 -0300

    feat: enhance SingUpPage with stateful widget, background image, and logo integration for improved user experience

--- lib/src/presentation/screens/sing_up/sing_up_page.dart
+++ lib/src/presentation/screens/sing_up/sing_up_page.dart
@@ -1,3 +1,4 @@
+import 'package:aurea_app/src/presentation/widgets/logo/logo_widget.dart';
 import 'package:flutter/material.dart';
 
 class SingUpPage extends StatelessWidget {
@@ -9,22 +10,78 @@ class SingUpPage extends StatelessWidget {
   }
 }
 
-class SingUpPageView extends StatelessWidget {
+class SingUpPageView extends StatefulWidget {
   const SingUpPageView({super.key});
 
   @override
+  State<SingUpPageView> createState() => _SingUpPageViewState();
+}
+
+class _SingUpPageViewState extends State<SingUpPageView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _pageController;
+
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text('Sing Up Page'),
-      ),
-      body: Column(
+      body: Stack(
         children: [
-          Text('Sing Up Page'),
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/smile_woman.jpg',
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+          ),
+          Positioned(
+            left: 0,
+            top: 0,
+            bottom: 0,
+            child: Container(
+              color: Colors.black.withValues(alpha: 0.5),
+              width: size.width * 0.7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40),
+                    child: BackButton(color: Colors.white),
+                  ),
+                  Column(children: [LogoWidget()]),
+                  SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
   }
 }
