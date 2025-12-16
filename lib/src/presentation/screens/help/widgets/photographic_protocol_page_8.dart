5cba4a9ae0056ab0505767d270507ee428ddb761
Lucas Barreto <lucasbarretoluz@hotmail.com>
2025-12-16 11:28:51 -0300

    feat: add PhotographicProtocolPage8 with mannequin_retractor_side image and navigation buttons for enhanced visual presentation

---
+++ lib/src/presentation/screens/help/widgets/photographic_protocol_page_8.dart
@@ -0,0 +1,106 @@
+import 'package:aurea_app/src/presentation/screens/help/widgets/photographic_frame_view.dart';
+import 'package:aurea_app/src/presentation/widgets/buttons/navigation_buttons.dart';
+import 'package:aurea_app/src/presentation/widgets/containers/shadow_gradient_container.dart';
+import 'package:flutter/material.dart';
+
+class PhotographicProtocolPage8 extends StatelessWidget {
+  final int currentPage;
+  final VoidCallback onPrevious;
+  final VoidCallback onNext;
+  final Color colorGradient;
+
+  const PhotographicProtocolPage8({
+    super.key,
+    required this.currentPage,
+    required this.onPrevious,
+    required this.onNext,
+    required this.colorGradient,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 90,
+          child: PhotographicFrameView(
+            imageAsset: 'assets/images/mannequin_retractor_side.png',
+            imageScale: 1.0,
+            customPaintWidth: 600,
+            customPaintHeight: 700,
+            gradientTop: 310,
+            gradientLeft: 19,
+            gradientRight: 19,
+            gradientHeight: 200,
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: ShadowGradientContainer(
+            height: 230,
+            colorGradient: colorGradient,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Column(
+                  children: [
+                    const Text(
+                      'Fotografia Frontal',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontFamily: 'Montserrat',
+                        letterSpacing: 3.5,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Container(
+                      height: 2.5,
+                      width: 280,
+                      color: Colors.black,
+                      margin: const EdgeInsets.only(bottom: 16),
+                    ),
+                    const Text(
+                      'Paciente em um ângulo',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Montserrat',
+                      ),
+                    ),
+                    const Text(
+                      'de 45º com afastador',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Montserrat',
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 50,
+          left: 0,
+          right: 0,
+          child: NavigationButtons(
+            currentPage: currentPage,
+            onPrevious: onPrevious,
+            onNext: onNext,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
