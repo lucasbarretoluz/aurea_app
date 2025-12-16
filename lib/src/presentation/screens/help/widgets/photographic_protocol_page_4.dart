8d1d03653ebd6068082271dd41f3170962ae16ba
Lucas Barreto <lucasbarretoluz@hotmail.com>
2025-12-16 10:45:37 -0300

    feat: add mannequin_smile_front image and integrate PhotographicFrameView in PhotographicProtocolPage4 for improved visual presentation and navigation

--- lib/src/presentation/screens/help/widgets/photographic_protocol_page_4.dart
+++ lib/src/presentation/screens/help/widgets/photographic_protocol_page_4.dart
@@ -1,3 +1,4 @@
+import 'package:aurea_app/src/presentation/screens/help/widgets/photographic_frame_view.dart';
 import 'package:aurea_app/src/presentation/widgets/buttons/navigation_buttons.dart';
 import 'package:aurea_app/src/presentation/widgets/containers/shadow_gradient_container.dart';
 import 'package:flutter/material.dart';
@@ -22,6 +23,22 @@ class PhotographicProtocolPage4 extends StatelessWidget {
       alignment: Alignment.center,
       children: [
         Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 90,
+          child: PhotographicFrameView(
+            imageAsset: 'assets/images/mannequin_smile_front.png',
+            imageScale: 1.25,
+            customPaintWidth: 600,
+            customPaintHeight: 700,
+            gradientTop: 310,
+            gradientLeft: 19,
+            gradientRight: 19,
+            gradientHeight: 200,
+          ),
+        ),
+        Positioned(
           bottom: 0,
           left: 0,
           right: 0,
@@ -29,15 +46,57 @@ class PhotographicProtocolPage4 extends StatelessWidget {
             height: 230,
             colorGradient: colorGradient,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                NavigationButtons(
-                  currentPage: currentPage,
-                  onPrevious: onPrevious,
-                  onNext: onNext,
+                Column(
+                  children: [
+                    const Text(
+                      'Fotografia Frontal',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontFamily: 'Montserrat',
+                        letterSpacing: 3.5,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Container(
+                      height: 2.5,
+                      width: 280,
+                      color: Colors.black,
+                      margin: const EdgeInsets.only(bottom: 16),
+                    ),
+                    const Text(
+                      'Paciente de frente ',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Montserrat',
+                      ),
+                    ),
+                    const Text(
+                      'sorrindo',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Montserrat',
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
+          ),
+        ),
+        Positioned(
+          bottom: 50,
+          left: 0,
+          right: 0,
+          child: NavigationButtons(
+            currentPage: currentPage,
+            onPrevious: onPrevious,
+            onNext: onNext,
           ),
         ),
       ],
