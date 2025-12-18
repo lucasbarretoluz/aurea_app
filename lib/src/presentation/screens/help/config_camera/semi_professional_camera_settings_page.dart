c60c8e7efa6cc5cf9bf5728e03a420717d367c35
Lucas Barreto <lucasbarretoluz@hotmail.com>
2025-12-17 22:23:15 -0300

    feat: convert SemiProfessionalCameraSettingsPage to StatefulWidget, implement page navigation with buttons for improved user interaction

--- lib/src/presentation/screens/help/config_camera/semi_professional_camera_settings_page.dart
+++ lib/src/presentation/screens/help/config_camera/semi_professional_camera_settings_page.dart
@@ -5,11 +5,56 @@ import 'package:aurea_app/src/presentation/screens/help/config_camera/widgets/se
 import 'package:aurea_app/src/presentation/screens/help/config_camera/widgets/semi_professional_camera_settings_page_4.dart';
 import 'package:aurea_app/src/presentation/screens/help/config_camera/widgets/semi_professional_camera_settings_page_5.dart';
 import 'package:aurea_app/src/presentation/screens/help/config_camera/widgets/semi_professional_camera_settings_page_6.dart';
+import 'package:aurea_app/src/presentation/widgets/buttons/navigation_buttons.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui';
 
-class SemiProfessionalCameraSettingsPage extends StatelessWidget {
+class SemiProfessionalCameraSettingsPage extends StatefulWidget {
   const SemiProfessionalCameraSettingsPage({super.key});
+
+  @override
+  State<SemiProfessionalCameraSettingsPage> createState() =>
+      _SemiProfessionalCameraSettingsPageState();
+}
+
+class _SemiProfessionalCameraSettingsPageState
+    extends State<SemiProfessionalCameraSettingsPage> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+  static const int _totalPages = 6;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      final page = _pageController.page?.round() ?? 0;
+      if (page != _currentPage) {
+        setState(() {
+          _currentPage = page;
+        });
+      }
+    });
+  }
+
+  void _goToPreviousPage() {
+    if (_currentPage > 0) {
+      _pageController.previousPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
+  void _goToNextPage() {
+    if (_currentPage < _totalPages - 1) {
+      _pageController.nextPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    } else {
+      Navigator.of(context).pop();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,14 +86,36 @@ class SemiProfessionalCameraSettingsPage extends StatelessWidget {
                   topRight: Radius.circular(30),
                 ),
               ),
-              child: PageView(
+              child: Column(
                 children: [
-                  const SemiProfessionalCameraSettingsPage1(),
-                  const SemiProfessionalCameraSettingsPage2(),
-                  const SemiProfessionalCameraSettingsPage3(),
-                  const SemiProfessionalCameraSettingsPage4(),
-                  const SemiProfessionalCameraSettingsPage5(),
-                  const SemiProfessionalCameraSettingsPage6(),
+                  Expanded(
+                    child: PageView(
+                      controller: _pageController,
+                      children: [
+                        const SemiProfessionalCameraSettingsPage1(),
+                        const SemiProfessionalCameraSettingsPage2(),
+                        const SemiProfessionalCameraSettingsPage3(),
+                        const SemiProfessionalCameraSettingsPage4(),
+                        const SemiProfessionalCameraSettingsPage5(),
+                        const SemiProfessionalCameraSettingsPage6(),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 0.0,
+                      vertical: 40.0,
+                    ),
+                    child: NavigationButtons(
+                      currentPage: _currentPage,
+                      totalPages: _totalPages,
+                      onPrevious: _goToPreviousPage,
+                      onNext: _goToNextPage,
+                      colorText: Colors.white,
+                      colorIcon: Colors.white,
+                      colorBackground: Color(0xffaea79d).withOpacity(0.26),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -56,5 +123,11 @@ class SemiProfessionalCameraSettingsPage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 }
