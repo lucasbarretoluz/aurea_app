6cb38dbebf68b9a7afa42bb930aa7e2f3b547161
Lucas Barreto <lucasbarretoluz@hotmail.com>
2025-12-19 21:21:58 -0300

    feat: add LinesAndReferencesPage1 widget for enhanced reference lines guidance in dental photography, including navigation buttons and a visual layout

---
+++ lib/src/presentation/screens/help/reference_lines/widgets/lines_and_references_page_1.dart
@@ -0,0 +1,118 @@
+import 'package:aurea_app/src/presentation/widgets/buttons/navigation_buttons.dart';
+import 'package:aurea_app/src/presentation/widgets/containers/shadow_gradient_container.dart';
+import 'package:flutter/material.dart';
+
+class LinesAndReferencesPage1 extends StatelessWidget {
+  final int currentPage;
+  final int totalPages;
+  final VoidCallback onPrevious;
+  final VoidCallback onNext;
+  final Color colorGradient;
+
+  const LinesAndReferencesPage1({
+    super.key,
+    required this.currentPage,
+    required this.totalPages,
+    required this.onPrevious,
+    required this.onNext,
+    required this.colorGradient,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: Transform.scale(
+            scale: 1.2,
+            child: Image.asset(
+              'assets/images/mannequin_smile_front.png',
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+        Positioned(
+          top: 110,
+          bottom: 100,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: Container(
+              width: 2,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: ShadowGradientContainer(
+            height: 250,
+            disableShadow: true,
+            colorGradient: colorGradient,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Linha Média',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontFamily: 'Montserrat',
+                        letterSpacing: 3.5,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Container(
+                      height: 2.5,
+                      width: 220,
+                      color: Colors.black,
+                      margin: const EdgeInsets.only(bottom: 16),
+                    ),
+                    const Text(
+                      'Linha vertical passando bem no ',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Montserrat',
+                      ),
+                    ),
+                    const Text(
+                      'meio da face do paciente',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Montserrat',
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 50,
+          left: 0,
+          right: 0,
+          child: NavigationButtons(
+            currentPage: currentPage,
+            totalPages: totalPages,
+            onPrevious: onPrevious,
+            onNext: onNext,
+          ),
+        ),
+      ],
+    );
+  }
+}
