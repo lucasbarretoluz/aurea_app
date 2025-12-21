d3832e931548f865621d613408386d938581e453
Lucas Barreto <lucasbarretoluz@hotmail.com>
2025-12-21 09:33:48 -0300

    feat: add LinesAndReferencesPage4 widget for enhanced reference lines guidance, featuring responsive layout and integrated navigation buttons

---
+++ lib/src/presentation/screens/help/reference_lines/widgets/lines_and_references_page_4.dart
@@ -0,0 +1,123 @@
+import 'package:aurea_app/src/presentation/widgets/buttons/navigation_buttons.dart';
+import 'package:aurea_app/src/presentation/widgets/containers/shadow_gradient_container.dart';
+import 'package:flutter/material.dart';
+
+class LinesAndReferencesPage4 extends StatelessWidget {
+  final int currentPage;
+  final int totalPages;
+  final VoidCallback onPrevious;
+  final VoidCallback onNext;
+  final Color colorGradient;
+
+  const LinesAndReferencesPage4({
+    super.key,
+    required this.currentPage,
+    required this.totalPages,
+    required this.onPrevious,
+    required this.onNext,
+    required this.colorGradient,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: Transform.scale(
+            scale: 1.2,
+            child: Image.asset(
+              'assets/images/mouth_front.png',
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+        Positioned(
+          top: screenHeight * 0.15,
+          bottom: screenHeight * 0.32,
+          left: 0,
+          right: 0,
+          child: Center(child: Container(width: 2, color: Colors.white)),
+        ),
+        Positioned(
+          top: screenHeight * 0.435,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: Container(
+              height: 2,
+              width: screenWidth * 0.8,
+              color: Colors.white,
+            ),
+          ),
+        ),
+
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: ShadowGradientContainer(
+            height: 250,
+            disableShadow: true,
+            colorGradient: colorGradient,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Sorriso',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontFamily: 'Montserrat',
+                        letterSpacing: 3.5,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Container(
+                      height: 2.5,
+                      width: 220,
+                      color: Colors.black,
+                      margin: const EdgeInsets.only(bottom: 16),
+                    ),
+                    const Text(
+                      'Posicione a linha horizontal sobre os \nzênites gengivais dos incisivos centrais e \ncaninos',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Montserrat',
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 50,
+          left: 0,
+          right: 0,
+          child: NavigationButtons(
+            currentPage: currentPage,
+            totalPages: totalPages,
+            onPrevious: onPrevious,
+            onNext: onNext,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
