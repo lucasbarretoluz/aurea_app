4ff291787fcb5bb50ad3162ba036dbfefee35d09
Lucas Barreto <lucasbarretoluz@hotmail.com>
2025-12-22 09:46:00 -0300

    feat: add DentalProportionsPage2 widget with navigation buttons and new smile images for enhanced user interaction

---
+++ lib/src/presentation/screens/help/dental_proportions/widgets/dental_proportions_page_2.dart
@@ -0,0 +1,100 @@
+import 'package:aurea_app/src/presentation/widgets/buttons/navigation_buttons.dart';
+import 'package:aurea_app/src/presentation/widgets/containers/shadow_gradient_container.dart';
+import 'package:flutter/material.dart';
+
+class DentalProportionsPage2 extends StatelessWidget {
+  final int currentPage;
+  final int totalPages;
+  final VoidCallback onPrevious;
+  final VoidCallback onNext;
+  final Color colorGradient;
+
+  const DentalProportionsPage2({
+    super.key,
+    required this.currentPage,
+    required this.totalPages,
+    required this.onPrevious,
+    required this.onNext,
+    required this.colorGradient,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 100,
+          child: Transform.scale(
+            scale: 1.0,
+            child: Image.asset(
+              'assets/images/smile_gingival_contour.png',
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: ShadowGradientContainer(
+            height: 250,
+            disableShadow: true,
+            colorGradient: colorGradient,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Contorno Gengival',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontFamily: 'Montserrat',
+                        letterSpacing: 3.5,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Container(
+                      height: 2.5,
+                      width: 220,
+                      color: Colors.black,
+                      margin: const EdgeInsets.only(bottom: 16),
+                    ),
+                    const Text(
+                      'Posicione a linha horizontal sobre os \nzênites gengivais dos incisivos centrais e \ncaninos',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Montserrat',
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 50,
+          left: 0,
+          right: 0,
+          child: NavigationButtons(
+            currentPage: currentPage,
+            totalPages: totalPages,
+            onPrevious: onPrevious,
+            onNext: onNext,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
